b7aa7ce1364f3257bd9fb1bd3a291c47faf74735
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-06-11 21:28:57 -0700

    Debug env clauses and comment out things for dev

--- scripts/run_everything.sh
+++ scripts/run_everything.sh
@@ -8,25 +8,43 @@ ENVNAME='crsd'
 if [[ "$CONDA_DEFAULT_ENV" == "$ENVNAME" ]]; then
     echo "Environment '$ENVNAME' already active"
 # If not active, but exists, activate
-elif conda info --envs | grep -q "$ENVNAME"
+# elif conda info --envs | grep -q "$ENVNAME"
+else
     echo "Activating '$ENVNAME'"
     conda activate "$ENVNAME"
-# Otherwise 
-else
-    echo "Environment '$ENVNAME' does not exist - exiting"
-    exit
 fi
+# Otherwise 
+# else
+#     echo "Environment '$ENVNAME' does not exist - exiting"
+#     exit
+# fi
+
+PPDIR='../data/1_preprocessed'
 
 # Pressure Transducers Initial Processing
 STRESS_RAW_FILE='../data/0_raw/Timeseries/RS_RAWdata_OSC_N.mat'
-STRESS_PUTC_FILE='../data/1_preprocessed/Timeseries/PhysicalUnitsData__UTC_Timing.csv'
-python ../processing/initial/CRSD_volts2phys.py -i $STRESS_RAW_FILE -o $STRESS_PUTC_FILE
+STRESS_PUTC_FILE="'$PPDIR'/Timeseries/PhysicalUnitsData__UTC_Timing.csv"
+#python ../processing/initial/CRSD_volts2phys.py -i $STRESS_RAW_FILE -o $STRESS_PUTC_FILE
 
 # LVDT Initial Processing
 LVDT_RAW_FILES='../data/0_raw/Timeseries/LVDT/*.txt'
-LVDT_UTC_FILE='../data/1_preprocessed/Timeseries/Stitched_LVDT_Data__UTC_Timing.csv'
-python ../processing/initial/merge_raw_LVDT.py -i $LVDT_RAW_FILES -o $LVDT_UTC_FILE -t 0.05 -r 1
+LVDT_UTC_FILE="'$PPDIR'/Timeseries/Stitched_LVDT_Data__UTC_Timing.csv"
+#python ../processing/initial/merge_raw_LVDT.py -i $LVDT_RAW_FILES -o $LVDT_UTC_FILE -t 0.05 -r 1
 
+# Timeseries resampling
+STRESS_RESAMPLED="'$PPDIR'/Timeseries/Resampled_Pressure_Data.csv"
+LVDT_RESAMPLED="'$PPDIR'/Timeseries/Resampled_LVDT_Data.csv"
+#python ../processing/preprocess/evenly_sample_timeseries.py -i $STRESS_PUTC_FILE -o $STRESS_RESAMPLED
+#python ../processing/preprocess/evenly_sample_timeseries.py -i $LVDT_UTC_FILE -o $LVDT_RESAMPLED
 
-echo "Will run processing scripts here"
+# Timeseries despiking
+STRESS_DESPIKED="'$PPDIR'/Timeseries/Despiked_Pressure_Data.csv"
+LVDT_DESPIKED="'$PPDIR'/Timeseries/Despiked_LVDT_Data.csv"
+python ../processing/preprocess/despike_timeseries.py -i $STRESS_RESAMPLED -o $STRESS_DESPIKED
+python ../processing/preprocess/despike_timeseries.py -i $LVDT_RESAMPLED -o $LVDT_DESPIKED
+
+# Timeseries smoothing
+STRESS_SMOOTH="'$PPDIR'/Timeseries/Smoothed_Pressure_Data.csv"
+LVDT_SMOOTH="'$PPDIR'/Timeseries/Smoothed_LVDT_Data.csv"
+
 echo "Will run figure rendering scripts here"

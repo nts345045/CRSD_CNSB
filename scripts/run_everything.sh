36778d11c59f31fc8e30b6bd2cda68b1afd24752
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-07-01 10:57:56 -0700

    WIP

--- scripts/run_everything.sh
+++ scripts/run_everything.sh
@@ -32,24 +32,24 @@ PPDIR='../data/1_preprocessed'
 # # Pressure Transducers Raw Data Ingestion
 STRESS_RAW_FILE='../data/0_raw/Timeseries/RS_RAWdata_OSC_N.mat'
 STRESS_PUTC_FILE="$PPDIR/Timeseries/PhysicalUnitsData__UTC_Timing.csv"
-python ../processing/ingestion/CRSD_volts2phys.py -i $STRESS_RAW_FILE -o $STRESS_PUTC_FILE
+# python ../processing/ingestion/CRSD_volts2phys.py -i $STRESS_RAW_FILE -o $STRESS_PUTC_FILE
 
 # # LVDT Raw Data Ingestion
 LVDT_RAW_FILES='../data/0_raw/Timeseries/LVDT/*.txt'
 LVDT_UTC_FILE="$PPDIR/Timeseries/Stitched_LVDT_Data__UTC_Timing.csv"
-python ../processing/ingestion/merge_raw_LVDT.py -i $LVDT_RAW_FILES -o $LVDT_UTC_FILE -t 0.05 -r 1
+# python ../processing/ingestion/merge_raw_LVDT.py -i $LVDT_RAW_FILES -o $LVDT_UTC_FILE -t 0.05 -r 1
 
 # # Timeseries sampling homogenization
 STRESS_RESAMPLED="$PPDIR/Timeseries/Resampled_Pressure_Data.csv"
 LVDT_RESAMPLED="$PPDIR/Timeseries/Resampled_LVDT_Data.csv"
-python ../processing/preprocessing/evenly_sample_timeseries.py -i $STRESS_PUTC_FILE -o $STRESS_RESAMPLED
-python ../processing/preprocessing/evenly_sample_timeseries.py -i $LVDT_UTC_FILE -o $LVDT_RESAMPLED
+# python ../processing/preprocessing/evenly_sample_timeseries.py -i $STRESS_PUTC_FILE -o $STRESS_RESAMPLED
+# python ../processing/preprocessing/evenly_sample_timeseries.py -i $LVDT_UTC_FILE -o $LVDT_RESAMPLED
 
 # # Timeseries despiking
 STRESS_DESPIKED="$PPDIR/Timeseries/Despiked_Pressure_Data.csv"
 LVDT_DESPIKED="$PPDIR/Timeseries/Despiked_LVDT_Data.csv"
-python ../processing/preprocessing/despike_timeseries.py -i $STRESS_RESAMPLED -o $STRESS_DESPIKED
-python ../processing/preprocessing/despike_timeseries.py -i $LVDT_RESAMPLED -o $LVDT_DESPIKED
+# python ../processing/preprocessing/despike_timeseries.py -i $STRESS_RESAMPLED -o $STRESS_DESPIKED
+# python ../processing/preprocessing/despike_timeseries.py -i $LVDT_RESAMPLED -o $LVDT_DESPIKED
 
 # # Timeseries smoothing
 STRESS_SMOOTH="$PPDIR/Timeseries/Smoothed_Pressure_Data.csv"

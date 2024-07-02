1a4212d0f60709c536073e594c3507b8a12bd039
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-07-02 08:16:51 -0700

    Flatten dir structure

--- scripts/run_everything.sh
+++ scripts/run_everything.sh
@@ -26,36 +26,36 @@ fi
 #     echo "Environment '$ENVNAME' does not exist - exiting"
 #     exit
 # fi
-
-PPDIR='../data/1_preprocessed'
-
+RDDIR='../data'
+PPDIR='../processed_data'
+SRC='../src'
 # # Pressure Transducers Raw Data Ingestion
-STRESS_RAW_FILE='../data/0_raw/Timeseries/RS_RAWdata_OSC_N.mat'
-STRESS_PUTC_FILE="$PPDIR/Timeseries/PhysicalUnitsData__UTC_Timing.csv"
-# python ../processing/ingestion/CRSD_volts2phys.py -i $STRESS_RAW_FILE -o $STRESS_PUTC_FILE
+STRESS_RAW_FILE="$RDDIR/RS_RAWdata_OSC_N.mat"
+STRESS_PUTC_FILE="$PPDIR/1_PhysicalUnitsData__UTC_Timing.csv"
+python $SRC/ingestion/CRSD_volts2phys.py -i $STRESS_RAW_FILE -o $STRESS_PUTC_FILE
 
 # # LVDT Raw Data Ingestion
-LVDT_RAW_FILES='../data/0_raw/Timeseries/LVDT/*.txt'
-LVDT_UTC_FILE="$PPDIR/Timeseries/Stitched_LVDT_Data__UTC_Timing.csv"
-# python ../processing/ingestion/merge_raw_LVDT.py -i $LVDT_RAW_FILES -o $LVDT_UTC_FILE -t 0.05 -r 1
+LVDT_RAW_FILES="$RDDIR/LVDT/*.txt"
+LVDT_UTC_FILE="$PPDIR/1_Stitched_LVDT_Data__UTC_Timing.csv"
+python $SRC/ingestion/merge_raw_LVDT.py -i $LVDT_RAW_FILES -o $LVDT_UTC_FILE -t 0.05 -r 1
 
 # # Timeseries sampling homogenization
-STRESS_RESAMPLED="$PPDIR/Timeseries/Resampled_Pressure_Data.csv"
-LVDT_RESAMPLED="$PPDIR/Timeseries/Resampled_LVDT_Data.csv"
-# python ../processing/preprocessing/evenly_sample_timeseries.py -i $STRESS_PUTC_FILE -o $STRESS_RESAMPLED
-# python ../processing/preprocessing/evenly_sample_timeseries.py -i $LVDT_UTC_FILE -o $LVDT_RESAMPLED
+STRESS_RESAMPLED="$PPDIR/2_Resampled_Pressure_Data.csv"
+LVDT_RESAMPLED="$PPDIR/2_Resampled_LVDT_Data.csv"
+python $SRC/preprocessing/evenly_sample_timeseries.py -i $STRESS_PUTC_FILE -o $STRESS_RESAMPLED
+python $SRC/preprocessing/evenly_sample_timeseries.py -i $LVDT_UTC_FILE -o $LVDT_RESAMPLED
 
 # # Timeseries despiking
-STRESS_DESPIKED="$PPDIR/Timeseries/Despiked_Pressure_Data.csv"
-LVDT_DESPIKED="$PPDIR/Timeseries/Despiked_LVDT_Data.csv"
-# python ../processing/preprocessing/despike_timeseries.py -i $STRESS_RESAMPLED -o $STRESS_DESPIKED
-# python ../processing/preprocessing/despike_timeseries.py -i $LVDT_RESAMPLED -o $LVDT_DESPIKED
+STRESS_DESPIKED="$PPDIR/3_Despiked_Pressure_Data.csv"
+LVDT_DESPIKED="$PPDIR/3_Despiked_LVDT_Data.csv"
+python $SRC/preprocessing/despike_timeseries.py -i $STRESS_RESAMPLED -o $STRESS_DESPIKED
+python $SRC/preprocessing/despike_timeseries.py -i $LVDT_RESAMPLED -o $LVDT_DESPIKED
 
 # # Timeseries smoothing
-STRESS_SMOOTH="$PPDIR/Timeseries/Smoothed_Pressure_Data.csv"
-LVDT_SMOOTH="$PPDIR/Timeseries/Smoothed_LVDT_Data.csv"
-python ../processing/preprocessing/smooth_timeseries.py -i $STRESS_DESPIKED -o $STRESS_SMOOTH -n 3 -w 10
-python ../processing/preprocessing/smooth_timeseries.py -i $LVDT_DESPIKED -o $LVDT_SMOOTH -n 3 -w 10
+STRESS_SMOOTH="$PPDIR/4_Smoothed_Pressure_Data.csv"
+LVDT_SMOOTH="$PPDIR/4_Smoothed_LVDT_Data.csv"
+python $SRC/preprocessing/smooth_timeseries.py -i $STRESS_DESPIKED -o $STRESS_SMOOTH -n 3 -w 10
+python $SRC/preprocessing/smooth_timeseries.py -i $LVDT_DESPIKED -o $LVDT_SMOOTH -n 3 -w 10
 
 # # Split Timeseries into Experiments
 

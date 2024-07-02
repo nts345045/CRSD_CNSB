84f82a496103a3d2b950f6895427de63a174aaaa
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-07-02 12:29:56 -0700

    WIP - LVDT analysis

--- scripts/run_everything.sh
+++ scripts/run_everything.sh
@@ -29,34 +29,51 @@ fi
 RDDIR='../data'
 PPDIR='../processed_data'
 SRC='../src'
-# # Pressure Transducers Raw Data Ingestion
+EXP_TIMES='../param/UTC_experiment_times.csv'
+
+# Pressure Transducers Raw Data Ingestion
 STRESS_RAW_FILE="$RDDIR/RS_RAWdata_OSC_N.mat"
 STRESS_PUTC_FILE="$PPDIR/1_PhysicalUnitsData__UTC_Timing.csv"
-python $SRC/ingestion/CRSD_volts2phys.py -i $STRESS_RAW_FILE -o $STRESS_PUTC_FILE
+# echo "~~~~~~~~~~ INGESTING TRANSDUCER TIMESERIES ~~~~~~~~~~"
+# python $SRC/ingestion/CRSD_volts2phys.py -i $STRESS_RAW_FILE -o $STRESS_PUTC_FILE
 
-# # LVDT Raw Data Ingestion
+# LVDT Raw Data Ingestion
 LVDT_RAW_FILES="$RDDIR/LVDT/*.txt"
 LVDT_UTC_FILE="$PPDIR/1_Stitched_LVDT_Data__UTC_Timing.csv"
-python $SRC/ingestion/merge_raw_LVDT.py -i $LVDT_RAW_FILES -o $LVDT_UTC_FILE -t 0.05 -r 1
+# echo "~~~~~~~~~~ INGESTING LVDT TIMESERIES ~~~~~~~~~~"
+# python $SRC/ingestion/merge_raw_LVDT.py -i $LVDT_RAW_FILES -o $LVDT_UTC_FILE -t 0.05 -r 1
 
-# # Timeseries sampling homogenization
+# Timeseries sampling homogenization
 STRESS_RESAMPLED="$PPDIR/2_Resampled_Pressure_Data.csv"
 LVDT_RESAMPLED="$PPDIR/2_Resampled_LVDT_Data.csv"
-python $SRC/preprocessing/evenly_sample_timeseries.py -i $STRESS_PUTC_FILE -o $STRESS_RESAMPLED
-python $SRC/preprocessing/evenly_sample_timeseries.py -i $LVDT_UTC_FILE -o $LVDT_RESAMPLED
+# echo "~~~~~~~~~~ EVENLY SAMPLING TIMESERIES ~~~~~~~~~~"
+# python $SRC/preprocessing/evenly_sample_timeseries.py -i $STRESS_PUTC_FILE -o $STRESS_RESAMPLED -n 2
+# python $SRC/preprocessing/evenly_sample_timeseries.py -i $LVDT_UTC_FILE -o $LVDT_RESAMPLED -n 2
 
-# # Timeseries despiking
+# Timeseries despiking
 STRESS_DESPIKED="$PPDIR/3_Despiked_Pressure_Data.csv"
 LVDT_DESPIKED="$PPDIR/3_Despiked_LVDT_Data.csv"
-python $SRC/preprocessing/despike_timeseries.py -i $STRESS_RESAMPLED -o $STRESS_DESPIKED
-python $SRC/preprocessing/despike_timeseries.py -i $LVDT_RESAMPLED -o $LVDT_DESPIKED
+# echo "~~~~~~~~~~ DESPIKING TIMESERIES ~~~~~~~~~~"
+# python $SRC/preprocessing/despike_timeseries.py -i $STRESS_RESAMPLED -o $STRESS_DESPIKED
+# python $SRC/preprocessing/despike_timeseries.py -i $LVDT_RESAMPLED -o $LVDT_DESPIKED
 
-# # Timeseries smoothing
+# Timeseries smoothing
 STRESS_SMOOTH="$PPDIR/4_Smoothed_Pressure_Data.csv"
 LVDT_SMOOTH="$PPDIR/4_Smoothed_LVDT_Data.csv"
-python $SRC/preprocessing/smooth_timeseries.py -i $STRESS_DESPIKED -o $STRESS_SMOOTH -n 3 -w 10
-python $SRC/preprocessing/smooth_timeseries.py -i $LVDT_DESPIKED -o $LVDT_SMOOTH -n 3 -w 10
+echo "~~~~~~~~~~ SMOOTHING TIMESERIES ~~~~~~~~~~"
+#Window settings both sample 10 minute moving windows that go out to +/- 4 sigma for gaussian weighted mean
+# python $SRC/preprocessing/smooth_timeseries.py -i $STRESS_DESPIKED -o $STRESS_SMOOTH -w 600 -s 75
+# python $SRC/preprocessing/smooth_timeseries.py -i $LVDT_DESPIKED -o $LVDT_SMOOTH -w 40 -s 5
 
-# # Split Timeseries into Experiments
+# Split Timeseries into Experiments
+SPLIT_DATA_PATH="$PPDIR/5_split_data"
+# echo "~~~~~~~~~~ SPLITTING OUT TIMESERIES ~~~~~~~~~~"
+# python $SRC/preprocessing/split_out_experiments.py -i $STRESS_SMOOTH -o $SPLIT_DATA_PATH -t "Presssure" -f $EXP_TIMES
+# python $SRC/preprocessing/split_out_experiments.py -i $LVDT_SMOOTH -o $SPLIT_DATA_PATH -t "LVDT" -f $EXP_TIMES
 
-echo "Will run figure rendering scripts here"
+echo "!!!!!!!!!! END OF PREPROCESSING !!!!!!!!!!"
+
+# Process LVDT for empirical melt-rate calculations and getting extremum
+ 
+
+# echo "Will run figure rendering scripts here"
--- src/primary/analyze_cleaned_lvdt.py
+++ src/primary/analyze_cleaned_lvdt.py
@@ -6,11 +6,62 @@
 :purpose: 
 """
 
-import argparse, logging
+import argparse, logging, glob, os, sys
 import pandas as pd
 import numpy as np
 
 Logger = logging.getLogger('smooth_timeseries.py')
+
+def pick_extrema_indices(series,T=pd.Timedelta(6,unit='hour'),granularity=0.2):
+	if isinstance(series,pd.DataFrame):
+		series = series[series.columns[0]]
+
+	t0 = series.index.min()
+	tf = series.index.max()
+	ti = t0
+	min_ind = []; min_vals = []
+	max_ind = []; max_vals = []
+	while ti + T <= tf:
+		iS = series[(series.index >= ti) & (series.index <=ti + T)]
+		imax = iS.max(); Imax = iS.idxmax()
+		imin = iS.min(); Imin = iS.idxmin()
+		# If maximum value is greater than edge values
+		if imax > iS.values[0] and imax > iS.values[-1]:
+			# And maximum index is not already in the output list
+			if Imax not in max_ind:
+				max_ind.append(Imax)
+				max_vals.append(imax)
+		if imin < iS.values[0] and imin < iS.values[-1]:
+			if Imin not in min_ind:
+				min_ind.append(Imin)
+				min_vals.append(imin)
+
+		ti += T*granularity
+
+	return {'I_max':pd.DatetimeIndex(max_ind),'V_max':np.array(max_vals),'I_min':pd.DatetimeIndex(min_ind),'V_min':np.array(min_vals)}
+
+
+def fit_dateline(datetimes,values):
+	# Convert datetime array into total seconds relative to minimum datetime
+	tvsec = (datetimes - datetimes.min()).total_seconds()
+	# Get slope
+	mod = np.polyfit(tvsec,values,1)
+	return mod[0]
+
+
+def reduce_series_by_slope(series,slope,reference_time,reference_value):
+	if isinstance(series,pd.DataFrame):
+		series = series[series.columns[0]]
+	series = series.sort_index()
+	dt_ind = (series.index - reference_time).total_seconds()
+	poly = [slope,reference_value]
+	mfun = np.poly1d(poly)
+	y_hat = mfun(dt_ind)
+	y_red = series.values - y_hat
+	s_out = pd.Series(y_red,index=series.index,name='%s red'%(series.name))
+	return s_out
+
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -19,20 +70,57 @@ def main():
     )
     parser.add_argument(
         '-i',
-        '--input',
+        '--input_path',
         action='store',
-        dest='input_file',
+        dest='input_path',
         default=None,
         help='path and filename of file to smooth',
         type=str
     )
     parser.add_argument(
         '-o',
-        '--output',
+        '--output_path',
         action='store',
-        dest='output_file',
-        default='tmp_smoothed_data.csv',
+        dest='output_path',
+        default='tmp/processed_LVDT',
         help='path and filename of file to save results to',
         type=str
     )
-    # TODO: Migrate core processes from processing/primary/S4_process_LVDT.py
+
+    parser.add_argument(
+        '-f',
+        '--parameter_file',
+        action='store',
+        dest='params',
+        default=None,
+        help='parameter file for experiment-specific processing parameters',
+        type=str
+    )
+    args = parser.parse_args()
+    if args.params is None:
+        Logger.critical('No parameter file provided with flag -f. Quitting')
+        sys.exit(1)
+	else:
+	df_pf = pd.read_csv(args.params)
+    # Get names of LVDT files
+    flist = glob.glob(os.path.join(args.input_path,'*LVDT*.csv'))
+    print(flist)
+    breakpoint()
+    for _f in flist:
+        # Load experiment specific data
+        df = pd.read_csv(_f)
+        df.index = pd.to_datetime(df.Epoch_UTC)
+		
+        Logger.info(f'data loaded for {_f}')
+        # Experiment specific parameter file
+        IND = 
+        peaks = pick_extrema_indices()
+
+
+if __name__ == '__main__':
+    ch = logging.StreamHandler()                                                            
+    fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(fmt)
+    Logger.addHandler(ch)
+    Logger.setLevel(logging.INFO)
+    main()

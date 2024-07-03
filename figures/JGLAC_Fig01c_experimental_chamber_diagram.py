e9e2b1a08b64e3db83c2236a79174a4ec50cb7b2
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-07-03 15:45:01 -0700

    Update to command line compliant

--- figures/JGLAC_Fig01c_experimental_chamber_diagram.py
+++ figures/JGLAC_Fig01c_experimental_chamber_diagram.py
@@ -8,22 +8,14 @@ This script produces a SIMPLE 3-D rendering of the UW-Madison cryogenic ring-she
 :VERSION: 0.0
 :LAST EDIT: August 6. 2021
 """
-
+import argparse, logging, os
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-w = 4. 		# Wavenumber
-ri = 0.1	# [m] Inner Radius of Experimental Chamber
-ro = 0.3 	# [m] Outer Radius of Experimental Chamber
-rc = (ri + ro)/2
-dr = (ro-ri)/200
-dT = (2*np.pi)/200
-ai = .025/2	# [m] Inner Amplitude of Sinusoidal Bed
-ao = .076/2	# [m] Outer Amplitude of Sinusoidal Bed
-h0 = 0.0  	# [m] 
-h1 = 0.27   # [m] 
-dh = (h1 - h0)/200
+Logger = logging.getLogger('Figure 1C Render')
+
+
 
 def thetaR2XY(theta,r):
 	x,y = r*np.cos(theta), r*np.sin(theta)
@@ -74,77 +66,158 @@ def generate_ring(r,dT,h0,dh):
 	R = np.ones(T.shape)*r
 	return R,T,H
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(
+		prog='JGLAC_Fig01c_experimental_chamber_diagram.py',
+		description='Renders Figure 1C for Stevens et al. (In Prep for J. Glac.)'
+	)
+	parser.add_argument(
+		'-o',
+		'--output_file',
+		action='store',
+		dest='fname',
+		default='../results/figures/JGLAC_Fig01c',
+		help='path and name to save the rendered figure to, minus extension (use -x for extension). Defaults to "../results/figures/JGLAC_Fig01c"',
+		type=str
+	)
+
+	parser.add_argument(
+		'-x',
+		'-extension',
+		action='store',
+		dest='extension',
+		default='png',
+		choices=['png','pdf','svg'],
+		help='the figure output extension (e.g., *.png, *.pdf, *.svg) callable by :meth:`~matplotlib.pyplot.savefig`. Defaults to "png"',
+		type=str
+	)
+
+	parser.add_argument(
+		'-r',
+		'--resolution',
+		action='store',
+		dest='dpi',
+		default='figure',
+		help='set the `dpi` argument for :meth:`~matplotlib.pyplot.savefig. Defaults to "figure"'
+	)
+
+	parser.add_argument(
+		'-s',
+		'--show',
+		action='store_true',
+		dest='show',
+		help='if included, render the figure on the desktop in addition to saving to disk'
+	)
+
+	args = parser.parse_args()
+
+	w = 4. 		# Wavenumber of installed bed
+	ri = 0.1	# [m] Inner Radius of Experimental Chamber
+	ro = 0.3 	# [m] Outer Radius of Experimental Chamber
+	rc = (ri + ro)/2 # [m] centerline radius
+	dr = (ro-ri)/200 # [m] radial element lengths
+	dT = (2*np.pi)/200 # [rad] angular element lengths
+	ai = .025/2	# [m] Inner Amplitude of Sinusoidal Bed
+	ao = .076/2	# [m] Outer Amplitude of Sinusoidal Bed
+	h0 = 0.0  	# [m] Bottom elevation of experimetal chamber
+	h1 = 0.27   # [m] Top elevation of experimental chamber
+	dh = (h1 - h0)/200 # [m] hight element lengths
+
+	fig = plt.figure(figsize=(6,4))
+	ax = fig.add_subplot(projection='3d')
 
 
-# Generate Bed Geometry
-A,R,T = generate_sinusoidal_bed(w,ri,ro,ai,ao,dr,dT)
-X,Y = thetaR2XY(T,R)
-ax.plot_surface(X,Y,A,color='gray',alpha=0.8)
-# Generate Centerline Trace
-A,R,T = generate_sinusoidal_trace(w,rc,ri,ro,ai,ao,dT)
-X,Y = thetaR2XY(T,R)
-IDX = (R <= rc*1.0001) & (R >= 0.9999)
-ax.plot_surface(X[IDX],Y[IDX],A[IDX],color='gray',alpha=0.8)
+	# Generate Bed Geometry
+	A,R,T = generate_sinusoidal_bed(w,ri,ro,ai,ao,dr,dT)
+	X,Y = thetaR2XY(T,R)
+	ax.plot_surface(X,Y,A,color='gray',alpha=0.8)
+	# Generate Centerline Trace
+	A,R,T = generate_sinusoidal_trace(w,rc,ri,ro,ai,ao,dT)
+	X,Y = thetaR2XY(T,R)
+	# IDX = (R <= rc*1.0001) & (R >= 0.9999)
+	# ax.plot_surface(X[IDX],Y[IDX],A[IDX],color='gray',alpha=0.8)
+	ax.plot_surface(X,Y,A+0.01,color='cyan',alpha=0.8)
 
-# Generate Cylinder Lower, Outer
-R,T,H = generate_cylinder_surface(ro,h0,h1,ao/20,dT)
-X,Y = thetaR2XY(T,R)
-ax.plot_surface(X,Y,H,color='dodgerblue',alpha=0.25)
-# Generate Cylinder Lower, Inner
-R,T,H = generate_cylinder_surface(ri,h0,h1,ao/20,dT)
-X,Y = thetaR2XY(T,R)
-ax.plot_surface(X,Y,H,color='dodgerblue',alpha=0.5)
-# Generate Floor
-R,T,Z = generate_circle_surface(0,ri,ro,dr,dT)
-X,Y = thetaR2XY(T,R)
-ax.plot_surface(X,Y,Z,color='darkgrey',alpha=0.75)
-# Generate Roof
-R,T,Z = generate_circle_surface(h1,ri,ro,dr,dT)
-X,Y = thetaR2XY(T,R)
-ax.plot_surface(X,Y,Z,color='dodgerblue',alpha=0.25)
+	# Generate Cylinder Lower, Outer
+	R,T,H = generate_cylinder_surface(ro,h0,h1,ao/20,dT)
+	X,Y = thetaR2XY(T,R)
+	ax.plot_surface(X,Y,H,color='dodgerblue',alpha=0.25)
+	# Generate Cylinder Lower, Inner
+	R,T,H = generate_cylinder_surface(ri,h0,h1,ao/20,dT)
+	X,Y = thetaR2XY(T,R)
+	ax.plot_surface(X,Y,H,color='dodgerblue',alpha=0.5)
+	# Generate Floor
+	R,T,Z = generate_circle_surface(0,ri,ro,dr,dT)
+	X,Y = thetaR2XY(T,R)
+	ax.plot_surface(X,Y,Z,color='darkgrey',alpha=0.75)
+	# Generate Roof
+	R,T,Z = generate_circle_surface(h1,ri,ro,dr,dT)
+	X,Y = thetaR2XY(T,R)
+	ax.plot_surface(X,Y,Z,color='dodgerblue',alpha=0.25)
 
-# Generate outer reference ring
-R,T,H = generate_ring(ro,dT,ao,0.001)
-X,Y = thetaR2XY(T,R)
-ax.plot_surface(X,Y,H,color='k',alpha=1)
-# Generate centerline reference ring
-R,T,H = generate_ring(ri,dT,ao,0.001)
-X,Y = thetaR2XY(T,R)
-ax.plot_surface(X,Y,H,color='k',alpha=1)
-# Generate inner reference ring
-R,T,H = generate_ring(ri,dT,ao,0.001)
-X,Y = thetaR2XY(T,R)
-ax.plot_surface(X,Y,H,color='k',alpha=1)
-
-
-# Plot example slice
-theta = np.pi*(1.5 + 1/(2*w))
-R,H,T = generate_radial_slice(theta,ri - .1*ro,ro*1.1,dr,h0-(h1*.25),h1*1.25,dh)
-X,Y = thetaR2XY(T,R)
-idx = [0,-1,0,-1],[0,0,-1,-1]
-ax.plot_surface(X[idx].reshape(2,2),Y[idx].reshape(2,2),H[idx].reshape(2,2),color='red',alpha=0.25)
-
-# Set Scale
-ax.set_xlim([-(ro+ri),(ro+ri)])
-ax.set_ylim([-(ro+ri),(ro+ri)])
-ax.set_zlim([-(ro+ri)+(h1 - h0)/2,(ro+ri)+(h1 - h0)/2])
-ax.set_xlabel('X [m]')
-ax.set_ylabel('Y [m]')
-ax.set_zlabel('Z [m]')
+	# Generate outer reference ring
+	R,T,H = generate_ring(ro,dT,ao,0.001)
+	X,Y = thetaR2XY(T,R)
+	ax.plot_surface(X,Y,H,color='k',alpha=1)
+	# Generate centerline reference ring
+	R,T,H = generate_ring(ri,dT,ao,0.001)
+	X,Y = thetaR2XY(T,R)
+	ax.plot_surface(X,Y,H,color='k',alpha=1)
+	# Generate inner reference ring
+	R,T,H = generate_ring(ri,dT,ao,0.001)
+	X,Y = thetaR2XY(T,R)
+	ax.plot_surface(X,Y,H,color='k',alpha=1)
 
 
-### PLOT CROSS-SECTION
-plt.figure()
-plt.fill_between([ri,ro],[ao-ai,0],[h1,h1],alpha=0.5,color='dodgerblue')
-plt.fill_between([ri,ro],[0,0],[ao-ai,0],alpha=0.5,color='darkgrey')
-plt.fill_between([ri,ro],[0,0],[ai + ao,2*ao],alpha=0.5,color='darkgrey')
-plt.plot([ri,ro],[ao,ao],'k-')
-plt.plot(np.ones(2)*(ro+ri)/2,[ao,ao+(ao+ai)/2],'k-')
-plt.plot([ri,ri,ro,ro],[h1,0,0,h1],'k-',linewidth=2)
-plt.xlim([0,ro*1.1])
-plt.xlabel('Radius [m]')
-plt.ylabel('Z [m]')
+	# # Plot example slice
+	# theta = np.pi*(1.5 + 1/(2*w))
+	# R,H,T = generate_radial_slice(theta,ri - .1*ro,ro*1.1,dr,h0-(h1*.25),h1*1.25,dh)
+	# X,Y = thetaR2XY(T,R)
+	# idx = [0,-1,0,-1],[0,0,-1,-1]
+	# ax.plot_surface(X[idx].reshape(2,2),Y[idx].reshape(2,2),H[idx].reshape(2,2),color='red',alpha=0.25)
 
-plt.show()
+	# Set Scale
+	# ax.set_xlim([-(ro+ri),(ro+ri)])
+	# ax.set_ylim([-(ro+ri),(ro+ri)])
+	# ax.set_zlim([-(ro+ri)+(h1 - h0)/2,(ro+ri)+(h1 - h0)/2])
+	ax.set_xlim([-ro*0.8, ro*0.8])
+	ax.set_ylim([-ro*0.8, ro*0.8])
+	ax.set_zlim([-(ro*0.8) + dh*100, ro*0.8 + dh*100])
+	ax.set_xlabel('X [m]')
+	ax.set_ylabel('Y [m]')
+	ax.set_zlabel('Z [m]')
+	ax.view_init(azim=-29, elev=16)
+
+	if args.dpi == 'figure':
+		dpi = 'figure'
+	else:
+		try:
+			dpi = float(args.dpi)
+
+		except:
+			dpi = 'figure'
+	if dpi == 'figure':
+		savename = f'{args.fname}_fdpi.{args.extension}'
+	else:
+		savename = f'{args.fname}_{dpi:.2f}dpi.{args.extension}'
+
+	if not os.path.exists(os.path.split(savename)[0]):
+		os.makedirs(os.path.split(savename)[0])
+	plt.savefig(savename, dpi=dpi, format=args.extension)
+
+	# ### PLOT CROSS-SECTION
+	# plt.figure()
+	# plt.fill_between([ri,ro],[ao-ai,0],[h1,h1],alpha=0.5,color='dodgerblue')
+	# plt.fill_between([ri,ro],[0,0],[ao-ai,0],alpha=0.5,color='darkgrey')
+	# plt.fill_between([ri,ro],[0,0],[ai + ao,2*ao],alpha=0.5,color='darkgrey')
+	# plt.plot([ri,ro],[ao,ao],'k-')
+	# plt.plot(np.ones(2)*(ro+ri)/2,[ao,ao+(ao+ai)/2],'k-')
+	# plt.plot([ri,ri,ro,ro],[h1,0,0,h1],'k-',linewidth=2)
+	# plt.xlim([0,ro*1.1])
+	# plt.xlabel('Radius [m]')
+	# plt.ylabel('Z [m]')
+
+
+	if args.show:
+		plt.show()

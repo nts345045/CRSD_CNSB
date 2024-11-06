6098f9b29397983fd8022152f70efff98df8bed2
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-11-06 15:12:02 -0800

    Initial commit

---
+++ src/util.py
@@ -0,0 +1,82 @@
+import logging
+
+class CriticalExitHandler(logging.Handler):
+    """A custom :class:`~logging.Handler` sub-class that emits a sys.exit
+    if a logging instance emits a logging.CRITICAL level message
+
+    Constructed through a prompt to ChatGPT and independently
+    tested by the author.
+
+    """
+    def __init__(self, exit_code=1, **kwargs):
+        super().__init__(**kwargs)
+        self.exit_code = exit_code
+        
+    def emit(self, record):
+        if record.levelno == logging.CRITICAL:
+            sys.exit(self.exit_code)
+
+def rich_error_message(e):
+    """Given the raw output of an "except Exception as e"
+    formatted clause, return a string that emulates
+    the error message print-out from python
+
+    e.g., 
+    'TypeError: input "input" is not type int'
+
+    :param e: Error object
+    :type e: _type_
+    :return: error message
+    :rtype: str
+    """    
+    etype = type(e).__name__
+    emsg = str(e)
+    return f'{etype}: {emsg}'
+
+# def setup_logger(name, level=logging.INFO, log_file=None, to_terminal=True):
+#     bckw = {'level': level}
+    
+#     if isinstance(log_file, str):
+#         bckw.update({'filename': log_file})
+    
+#     logging.basicConfig(filename = log_file, format='%(asctime)s - %(file)s'
+#     Logger = logging.getLogger(name)
+
+
+def setup_terminal_logger(name, level=logging.INFO):
+    """QuickStart setup for a write-to-command-line-only
+    logger that has safety catches against creating
+    multiple StreamHandlers with repeat calls of a
+    script in ipython or similar interactive python session.
+
+    :param name: name for the logger
+        e.g., __name__ is fairly customary
+    :type name: str
+    :param level: logging level, defaults to logging.INFO
+    :type level: int, optional
+    :return: Logger
+    :rtype: logging.RootLogger
+    """    
+    ## SET UP LOGGING
+    Logger = logging.getLogger(name)
+    # Set logging level to INFO
+    Logger.setLevel(level)
+
+    # Prevent duplication during testing
+    # Solution from https://stackoverflow.com/questions/31403679/python-logging-module-duplicated-console-output-ipython-notebook-qtconsole
+    # User Euclides (Sep 8, 2015)
+    handler_console = None
+    handlers = Logger.handlers
+    for h in handlers:
+        if isinstance(h, logging.StreamHandler):
+            handler_console = h
+            break
+    # Set up logging to terminal
+    if handler_console is None:
+        ch = logging.StreamHandler()
+        # Set up logging line format
+        fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(fmt)
+        # Add formatting & handler
+        Logger.addHandler(ch)
+    return Logger

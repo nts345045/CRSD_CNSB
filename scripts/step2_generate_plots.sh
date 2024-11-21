c910216496b733f2820898695862eff445c05d52
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-11-21 15:19:11 -0800

    Update script sequencing and flags

--- scripts/step2_generate_plots.sh
+++ scripts/step2_generate_plots.sh
@@ -29,30 +29,30 @@ SRC="$ROOT/src/figures"
 
 
 echo "~~~~~~~~~~ GENERATING PUBLICATION FIGURES ~~~~~~~~~~"
-# echo "Figure 1a - 3D render of sample chamber contents"
-# python $SRC/JGLAC_Fig01a.py -o $FIGURE_DIR -f 'png' -d 200
+echo "Figure 1a - 3D render of sample chamber contents"
+python $SRC/JGLAC_Fig01a.py -o $FIGURE_DIR -f 'png' -d 200
 
-# echo "Figure 2 - Steady State Parameter Space"
-# python $SRC/JGLAC_Fig02.py -i $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
+echo "Figure 2 - Steady State Parameter Space"
+python $SRC/JGLAC_Fig02.py -i $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
 
-# echo "Figure 3 - Effective Pressure Profile"
-# python $SRC/JGLAC_Fig03.py -i $PD_TIME_DIR/4_Smoothed_Pressure_Data.csv -o $FIGURE_DIR -f 'png' -d 200
+echo "Figure 3 - Effective Pressure Profile"
+python $SRC/JGLAC_Fig03.py -i $PD_TIME_DIR/4_Smoothed_Pressure_Data.csv -o $FIGURE_DIR -f 'png' -d 200
 
 echo "Figure 4 - Cavity geometry observation from T24"
 python $SRC/JGLAC_Fig04.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -m $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
 
-# echo "Figure 5 - Cavity geometry observation from T24"
-# python $SRC/JGLAC_Fig05.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -o $FIGURE_DIR -f 'png' -d 200
+echo "Figure 5 - Cavity geometry observation from T24"
+python $SRC/JGLAC_Fig05.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -o $FIGURE_DIR -f 'png' -d 200 -sr
 
-# echo "Figure 6 - Cavity geometry observation from T06"
-# python $SRC/JGLAC_Fig06.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -o $FIGURE_DIR -f 'png' -d 200
+echo "Figure 6 - Cavity geometry observation from T06"
+python $SRC/JGLAC_Fig06.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -o $FIGURE_DIR -f 'png' -d 200
 
-# echo "Figures 7 and 8 - Experiment T24 and T06 Crossplots"
-# python $SRC/JGLAC_Fig07.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -m $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
-# python $SRC/JGLAC_Fig08.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -m $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
+echo "Figures 7 and 8 - Experiment T24 and T06 Crossplots"
+python $SRC/JGLAC_Fig07.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -m $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
+python $SRC/JGLAC_Fig08.py -e $PD_EXP_DIR -g $PD_GEOM_DIR -m $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
 
-# echo "Figure 9 - Experiment T06 and T24 local stresses"
-# python $SRC/JGLAC_Fig09.py -g $PD_GEOM_DIR -o $FIGURE_DIR -f 'png' -d 200
+echo "Figure 9 - Experiment T06 and T24 local stresses"
+python $SRC/JGLAC_Fig09.py -g $PD_GEOM_DIR -m $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
 
-# echo "Figure 10 - Comparison of T06 and T24 to common sliding rule parameters from Schoof (2005) parameterizations"
-# python $SRC/JGLAC_Fig10.py -g $PD_GEOM_DIR -m $PD_MOD_DIR -o $FIGURE_DIR -f 'png' -d 200
+echo "Figure 10 - Comparison of T06 and T24 to common sliding rule parameters from Schoof (2005) parameterizations"
+python $SRC/JGLAC_Fig10.py -g $PD_GEOM_DIR -o $FIGURE_DIR -f 'png' -d 200
